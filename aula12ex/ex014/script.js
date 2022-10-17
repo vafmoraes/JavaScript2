474fea1adbc2be3b1f58ec6a3e7b79780661f93d
vafmoraes <112376376+vafmoraes@users.noreply.github.com>
2022-10-17 19:57:54 -0300

    ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert ('Ola!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours() //var hora = 17
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#b5c8cc'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#ffcda8'
+    } else {
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#7c609b'
+    }
+}
