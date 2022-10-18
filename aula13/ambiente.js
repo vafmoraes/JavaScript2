d51cd0cb534addbb83d6ba6c3563fe93232241a0
vafmoraes <112376376+vafmoraes@users.noreply.github.com>
2022-10-18 00:37:49 -0300

    Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* qualquer das estruturas emitem o mesmo resultado, mas a primeira é a mais inteligente para se usar (do = faça e while = enquanto)
+
+/*
+a mais encontrada dentro dos programas
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
